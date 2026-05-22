0374b182c2bfbf4eb8748c040bf061cf0198da26
Z User <z@container>
2026-05-22 21:39:28 +0000

    82460981-c498-4ff1-8d4a-f998aff7c289

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,6 +44,7 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
+        suppressHydrationWarning
       >
         {children}
         <Toaster />
